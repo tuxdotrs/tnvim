dda4d28db08c7d36f5a08f0b340149ba724e2df1
tux <t@tux.rs>
2024-11-14 07:18:33 +0530

    feat(awesomeWM): remove main menu

--- pkgs/awesome/awesome/ui/init.lua
+++ pkgs/awesome/awesome/ui/init.lua
@@ -1,6 +1,5 @@
 -- Returns all widgets, with assigned names, in a table.
 return {
-  menu = require(... .. ".menu"),
   notification = require(... .. ".notification"),
   titlebar = require(... .. ".titlebar"),
   wibar = require(... .. ".wibar"),
