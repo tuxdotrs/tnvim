6812ab9e7267e4b9bcfc244043371f846b799aa9
tux <t@tux.rs>
2024-11-29 13:25:23 +0530

    feat: use stable version of firefox from nixpkgs

--- modules/home-manager/firefox/default.nix
+++ modules/home-manager/firefox/default.nix
@@ -8,7 +8,7 @@
   programs.firefox = {
     enable = true;
 
-    package = pkgs.firefox.override {
+    package = pkgs.stable.firefox.override {
       extraPolicies = {
         CaptivePortal = false;
         DisableFirefoxStudies = true;
