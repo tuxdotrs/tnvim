f2026f99e7037271d375a9bc9a757a831b4c00e6
tux <t@tux.rs>
2024-12-07 08:22:08 +0530

    feat: add lazydocker

--- modules/nixos/virtualisation/docker.nix
+++ modules/nixos/virtualisation/docker.nix
@@ -1,7 +1,13 @@
-{username, ...}: {
+{
+  username,
+  pkgs,
+  ...
+}: {
   virtualisation = {
     docker.enable = true;
   };
+
+  environment.systemPackages = with pkgs; [lazydocker];
 
   users.users.${username}.extraGroups = ["docker"];
 }
