dc1b2ebeff81c172e3325324f0b4dc329c9dd3e0
tux <t@tux.rs>
2025-01-04 11:10:26 +0530

    feat: customize ghostty

--- modules/home-manager/ghostty/default.nix
+++ modules/home-manager/ghostty/default.nix
@@ -1,4 +1,18 @@
 {pkgs, ...}: {
+  home.file = {
+    ".config/ghostty/config" = {
+      text = ''
+        theme = rose-pine
+
+        gtk-titlebar = false
+        window-padding-x = 10
+        window-padding-y = 10
+
+        font-size = 16
+      '';
+    };
+  };
+
   home.packages = with pkgs; [
     ghostty
   ];
