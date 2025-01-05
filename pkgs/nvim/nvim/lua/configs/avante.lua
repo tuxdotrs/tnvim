0e79fd71bc04b58c5d2b9b0955ebcda7c7ddf655
tux <t@tux.rs>
2025-01-05 20:17:55 +0530

    feat(neovim): add hyperbolic and deepseek provider

--- pkgs/nvim/nvim/lua/configs/avante.lua
+++ pkgs/nvim/nvim/lua/configs/avante.lua
@@ -1,28 +1,20 @@
 local opts = {
-  provider = "ollama",
+  provider = "hyperbolic",
   vendors = {
-    ollama = {
-      ["local"] = true,
-      endpoint = "100.64.0.1:11434/v1",
-      model = "deepseek-coder-v2",
-      parse_curl_args = function(opts, code_opts)
-        return {
-          url = opts.endpoint .. "/chat/completions",
-          headers = {
-            ["Accept"] = "application/json",
-            ["Content-Type"] = "application/json",
-          },
-          body = {
-            model = opts.model,
-            messages = require("avante.providers").copilot.parse_message(code_opts),
-            max_tokens = 2048,
-            stream = true,
-          },
-        }
-      end,
-      parse_response_data = function(data_stream, event_state, opts)
-        require("avante.providers").openai.parse_response(data_stream, event_state, opts)
-      end,
+    deepseek = {
+      __inherited_from = "openai",
+      api_key_name = "DEEPSEEK_API_KEY",
+      endpoint = "https://api.deepseek.com/v1",
+      model = "deepseek-chat",
+    },
+
+    hyperbolic = {
+      __inherited_from = "openai",
+      api_key_name = "DEEPSEEK_API_KEY",
+      endpoint = "https://api.hyperbolic.xyz/v1",
+      model = "deepseek-ai/DeepSeek-V3",
+      temperature = 0,
+      max_tokens = 4096,
     },
   },
 }
