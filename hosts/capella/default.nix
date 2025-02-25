7526abde1d4bcf8b7c2f2de5a2233eaac95f79c7
tux <t@tux.rs>
2025-02-26 02:56:32 +0530

    feat: add impermanence

--- hosts/capella/default.nix
+++ hosts/capella/default.nix
@@ -2,49 +2,93 @@
   modulesPath,
   inputs,
   username,
+  lib,
   ...
 }: {
   imports = [
     (modulesPath + "/installer/scan/not-detected.nix")
     (modulesPath + "/profiles/qemu-guest.nix")
     inputs.disko.nixosModules.default
-    (import ./disko.nix {device = "/dev/sda";})
+    (import ./disko.nix {device = "/dev/vda";})
 
     ../common
-    ../../modules/nixos/virtualisation/docker.nix
-    ../../modules/nixos/selfhosted/containers/cs2.nix
   ];
 
   tux.services.openssh.enable = true;
 
-  sops.secrets = {
-    "cs2_secrets/SRCDS_TOKEN" = {
-      sopsFile = ./secrets.yaml;
+  nixpkgs = {
+    hostPlatform = "x86_64-linux";
+  };
+
+  boot = {
+    initrd.systemd = {
+      enable = lib.mkForce true;
+
+      services.wipe-my-fs = {
+        wantedBy = ["initrd.target"];
+        after = ["initrd-root-device.target"];
+        before = ["sysroot.mount"];
+        unitConfig.DefaultDependencies = "no";
+        serviceConfig.Type = "oneshot";
+        script = ''
+          mkdir /btrfs_tmp
+          mount /dev/disk/by-partlabel/disk-primary-root /btrfs_tmp
+
+          if [[ -e /btrfs_tmp/root ]]; then
+              mkdir -p /btrfs_tmp/old_roots
+              timestamp=$(date --date="@$(stat -c %Y /btrfs_tmp/root)" "+%Y-%m-%-d_%H:%M:%S")
+              mv /btrfs_tmp/root "/btrfs_tmp/old_roots/$timestamp"
+          fi
+
+          delete_subvolume_recursively() {
+              IFS=$'\n'
+              for i in $(btrfs subvolume list -o "$1" | cut -f 9- -d ' '); do
+                  delete_subvolume_recursively "/btrfs_tmp/$i"
+              done
+              btrfs subvolume delete "$1"
+          }
+
+          for i in $(find /btrfs_tmp/old_roots/ -maxdepth 1 -mtime +30); do
+              delete_subvolume_recursively "$i"
+          done
+
+          btrfs subvolume create /btrfs_tmp/root
+          umount /btrfs_tmp
+        '';
+      };
     };
 
-    "cs2_secrets/CS2_RCONPW" = {
-      sopsFile = ./secrets.yaml;
-    };
-
-    "cs2_secrets/CS2_PW" = {
-      sopsFile = ./secrets.yaml;
+    loader = {
+      grub = {
+        efiSupport = true;
+        efiInstallAsRemovable = true;
+      };
     };
   };
 
-  nixpkgs.hostPlatform = "x86_64-linux";
+  networking.hostName = "capella";
 
-  networking = {
-    hostName = "capella";
-    firewall.enable = false;
+  users = {
+    users.${username} = {
+      password = "${username}";
+      hashedPasswordFile = lib.mkForce null;
+    };
   };
 
-  boot.loader.grub = {
-    efiSupport = true;
-    efiInstallAsRemovable = true;
-  };
-
+  programs.fuse.userAllowOther = true;
+  fileSystems."/persist".neededForBoot = true;
   environment.persistence."/persist" = {
-    enable = false;
+    hideMounts = true;
+    directories = [
+      "/var/log"
+      "/var/lib/nixos"
+    ];
+    files = [
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+    ];
   };
 
   home-manager.users.${username} = {
--- hosts/capella/home.nix
+++ hosts/capella/home.nix
@@ -1,3 +1,14 @@
-{...}: {
+{username, ...}: {
+  home.persistence."/persist/home/${username}" = {
+    directories = [
+      "Projects"
+      ".ssh"
+    ];
+    files = [
+      ".zsh_history"
+    ];
+    allowOther = true;
+  };
+
   home.stateVersion = "24.11";
 }
